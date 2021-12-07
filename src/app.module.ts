02f0312df0122e03038326f8a182bfecc44dc22b
Adriano Lamas <lamas.free@gmail.com>
2021-12-07 20:57:30 -0300

    Update instalação prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { UsuarioModule } from './usuario/usuario.module';
+import { FilmesModule } from './filmes/filmes.module';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsuarioModule, FilmesModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
